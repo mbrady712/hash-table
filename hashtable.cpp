1400acaa802ffe6663046afb46061cde48a78080
Michael Brady <michaelbrady712@gmail.com>
2023-03-27 19:43:57 -0500

    Created skeleton of hashtable.cpp

--- hashtable.cpp
+++ hashtable.cpp
@@ -3,3 +3,33 @@ Name: Michael Brady
 Programming Fundamentals III
 Purpose: This program creates a hash table that implements separate chaining.
  */
+
+#include "hashtable.h"
+
+//Public methods
+
+bool HashTable::insertEntry(int id, string* Data){
+    return true;
+}
+
+string HashTable::getData(int id){
+    return "hello";
+}
+
+bool HashTable::removeEntry(int id){
+    return true;
+}
+
+int HashTable::getCount(){
+    return 1;
+}
+
+void HashTable::printTable(){
+
+}
+
+//Private methods
+
+int HashTable::hash(int id){
+    return 1;
+}
