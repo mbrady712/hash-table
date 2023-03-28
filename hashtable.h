b7d746ab48508e03b0b98aaa8ded630ca2eda882
Michael Brady <michaelbrady712@gmail.com>
2023-03-27 19:36:29 -0500

    Created preliminary header file for hashtable.cpp

--- hashtable.h
+++ hashtable.h
@@ -3,3 +3,26 @@ Name: Michael Brady
 Programming Fundamentals III
 Purpose: This program creates a hash table that implements separate chaining.
  */
+
+#ifndef HASH_TABLE
+#define HASH_TABLE
+
+#include "data.h"
+#include "linkedlist.h"
+#include <iostream>
+
+class HashTable {
+
+public:
+    bool insertEntry(int, string*);
+    string getData(int);
+    bool removeEntry(int);
+    int getCount();
+    void printTable();
+
+private:
+    int hash(int);
+    
+};
+
+#endif //HASH_TABLE
